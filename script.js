b239800c26043b02a23d21c59a37d03f7bc297ac
Sebastian Ossandon <sebastian.ossandon1149@gmail.com>
2023-03-29 10:34:16 -0300

    curriculum vitae

---
+++ script.js
@@ -0,0 +1,9 @@
+//Funcion que muestra el menu responsive
+function responsiveMenu() {
+    var x =document.getElementById("nav");
+        if (x.className === ""){
+            x.className = "responsive"
+        } else {
+            x.className = "";
+        }
+}
